35b4a91c88d15134e88ec8b8eb7f83c5f7b8dcfb
ruchi06 <ruchi06@gmail.com>
2023-01-08 19:56:32 +0530

    FlipcoinPersentage

---
+++ BasicProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter How Many Times You Have to Flip a Coin : ");
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            FlipCoin coin = new FlipCoin();
+            coin.getPercentageOfFlipCion(C);
+        }
+    }
+}
