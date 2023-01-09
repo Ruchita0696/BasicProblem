23e326f47115c485f68928f63ad3fe6ee4ffc4f3
ruchi06 <ruchi06@gmail.com>
2023-01-09 22:15:33 +0530

    Leap Year

--- BasicProblem/Program.cs
+++ BasicProblem/Program.cs
@@ -4,11 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter How Many Times You Have to Flip a Coin : ");
-            int C = Convert.ToInt32(Console.ReadLine());
-
-            FlipCoin coin = new FlipCoin();
-            coin.getPercentageOfFlipCion(C);
+            Console.WriteLine("Enter the Year to Check Whether Leap Year or Not : ");
+            int yeartoread = Convert.ToInt32(Console.ReadLine());
+            LeapYear Year = new LeapYear();
+            Year.toCheckLeapYear(yeartoread);
         }
     }
 }
