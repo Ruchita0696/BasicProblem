284b8310c974de646be5f046d074654ef6fbed62
ruchi06 <ruchi06@gmail.com>
2023-01-13 00:55:23 +0530

    Swap Two Number

--- BasicProblem/Program.cs
+++ BasicProblem/Program.cs
@@ -4,34 +4,13 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            Console.WriteLine("Enter the Index of 2 : ");
-            int Power = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 1 : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2 : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Power power = new Power();
-            power.getPowerOf2(Power);
-=======
-            Console.WriteLine("Enter the Number For Prime Factor : ");
-            int F = Convert.ToInt32(Console.ReadLine());
-
-            Factors factor = new Factors();
-            factor.PrimeFactors(F);
->>>>>>> UC5.Factors
-=======
-            Console.WriteLine("Enter the Year to Check Whether Leap Year or Not : ");
-            int yeartoread = Convert.ToInt32(Console.ReadLine());
-            LeapYear Year = new LeapYear();
-            Year.toCheckLeapYear(yeartoread);
->>>>>>> UC2.LeapYear
-=======
-            Console.WriteLine("Enter Number : ");
-            int H = Convert.ToInt32(Console.ReadLine());
-
-            HarmonicNumber Harmonic = new HarmonicNumber();
-            Harmonic.harmonicSeries(H);
->>>>>>> UC4.HarmonicNumber
+            Swapnumber swap = new Swapnumber();
+            swap.SwappingOfTwoNumbers(num1, num2);
         }
     }
 }
---
+++ BasicProblem/Swapnumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class Swapnumber
+    {
+        public void SwappingOfTwoNumbers(int Number1, int Number2)
+        {
+            int Number3 = Number1;
+            Number1 = Number2;
+            Number2 = Number3;
+
+            Console.WriteLine("Number 1 is : " + Number1);
+            Console.WriteLine("Number 2 is : " + Number2);
+        }
+    }
+}
