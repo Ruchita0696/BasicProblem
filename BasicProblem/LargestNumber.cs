e2649e96d7088d6a732efc252bf1af046bf60ec5
ruchi06 <ruchi06@gmail.com>
2023-01-13 21:38:00 +0530

    Largest Amoung Three Number

---
+++ BasicProblem/LargestNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem
+{
+    public class LargestNumber
+    {
+        public void CalculateLargestNumber(int Num1, int Num2, int Num3)
+        {
+            if (Num1 > Num2 && Num1 > Num3)
+            {
+                Console.WriteLine(Num1 + " is Largest Amoung All ");
+            }
+            else if (Num2 > Num3 && Num2 > Num3)
+            {
+                Console.WriteLine (Num2 + " is Largest Amoung All ")
+            }
+            else
+            {
+                Console.WriteLine(Num3 + " is Largest Amoung All");
+            }
+        }
+    }
+}
--- BasicProblem/Program.cs
+++ BasicProblem/Program.cs
@@ -4,12 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number to Check Even or Odd: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            
+            Console.WriteLine("To Find Largest Number Enter Three Numbers Below");
+            Console.WriteLine("Enter Number 1 : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2 : ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 3 : ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
 
-            EvenandOdd evenorodd = new EvenandOdd();
-            evenorodd.checkEvenOdd(num1);
+            LargestNumber largestNumber = new LargestNumber();
+            largestNumber.CalculateLargestNumber(n1, n2, n3);
         }
     }
 }
