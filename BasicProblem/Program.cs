4e116559f6cf1566e8c1497d25ee6684ad4ca1e3
ruchi06 <ruchi06@gmail.com>
2023-01-13 21:17:49 +0530

    Even Or Odd

--- BasicProblem/Program.cs
+++ BasicProblem/Program.cs
@@ -4,13 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number 1 : ");
+            Console.WriteLine("Enter Number to Check Even or Odd: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Number 2 : ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            
 
-            Swapnumber swap = new Swapnumber();
-            swap.SwappingOfTwoNumbers(num1, num2);
+            EvenandOdd evenorodd = new EvenandOdd();
+            evenorodd.checkEvenOdd(num1);
         }
     }
 }
